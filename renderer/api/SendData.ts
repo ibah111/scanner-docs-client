063a6db564117334d441ae5e741562381f32c6e1
Ivanova Maria <ivanova_ms@outlook.com>
2022-07-11 15:15:01 +0300

    отправка данных на сервер

--- renderer/api/SendData.ts
+++ renderer/api/SendData.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError } from "axios";
+import moment from "moment";
 import { store } from "../Reducer";
 import { callError } from "../Reducer/Message";
 import { getToken } from "../utils/getToken";
@@ -6,10 +7,17 @@ import server from "../utils/server";
 
 export default async function SendData() {
   const data = store.getState().Send;
+  const id = store.getState().Data.id;
+  const addons: any = {};
+  if (!data.DateSend) {
+    addons.DateSend = moment().toISOString();
+  }
   try {
     const result = await axios.post(server() + "/send", {
       ...getToken(),
       ...data,
+      ...addons,
+      id,
     });
     return result.data;
   } catch (e) {
