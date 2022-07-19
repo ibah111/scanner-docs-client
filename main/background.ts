a26f3014158cdf642439e2bdedc827b297dee08d
Ivanova Maria <ivanova_ms@outlook.com>
2022-07-19 17:32:12 +0300

    начало автообновления

--- main/background.ts
+++ main/background.ts
@@ -2,7 +2,7 @@ import { app, ipcMain } from "electron";
 import serve from "electron-serve";
 import { createWindow } from "./helpers";
 import events from "./events";
-
+import "./updater";
 const isProd: boolean = process.env.NODE_ENV === "production";
 if (isProd) {
   serve({ directory: "app" });
