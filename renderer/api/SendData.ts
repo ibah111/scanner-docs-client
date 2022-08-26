ac9f50a63079875545aa523bd0240fa39d0c1cde
Ivanova Maria <ivanova_ms@outlook.com>
2022-08-26 14:37:22 +0300

    скрытье формы отправки. при успешном отправлении данных

--- renderer/api/SendData.ts
+++ renderer/api/SendData.ts
@@ -15,6 +15,7 @@ export default async function SendData() {
       id,
     });
     store.dispatch(callSuccess("Данные успешно отправлены"));
+
     return result.data;
   } catch (e) {
     if (e instanceof AxiosError)
--- renderer/components/MainPage/sendingForm.tsx
+++ renderer/components/MainPage/sendingForm.tsx
@@ -10,67 +10,75 @@ import errorDate from "../../utils/errorDate";
 export default function SendingForm() {
   const data = useAppSelector((state) => state.Send);
   const errorDateText = errorDate(data.DateSend);
+  const [show, setShow] = React.useState(true);
   const dispatch = useAppDispatch();
   return (
-    <React.Fragment>
-      <Paper>
-        <Box
-          sx={{
-            bgcolor: "#cfe8fc",
-            height: "280px",
-            width: "470px",
-            "& .MuiTextField-root": { m: 1, width: "50ch" },
-          }}
-        >
-          <Typography variant="h4" textAlign={"center"}>
-            Отправляем в банк/ОСП
-          </Typography>
-          <DatePicker
-            label="Дата отправки"
-            value={data.DateSend}
-            onChange={(newValue: Moment) => {
-              dispatch(
-                setSend([
-                  "DateSend",
-                  newValue
-                    ? newValue.isValid()
-                      ? newValue.toISOString()
-                      : newValue.toString()
-                    : "",
-                ])
-              );
-            }}
-            renderInput={(params) => (
-              <TextField
-                id="date_send"
-                label="Дата"
-                {...params}
-                helperText={errorDateText}
-                variant="filled"
+    <>
+      {show ? (
+        <React.Fragment>
+          <Paper>
+            <Box
+              sx={{
+                bgcolor: "#cfe8fc",
+                height: "280px",
+                width: "470px",
+                "& .MuiTextField-root": { m: 1, width: "50ch" },
+              }}
+            >
+              <Typography variant="h4" textAlign={"center"}>
+                Отправляем в банк/ОСП
+              </Typography>
+              <DatePicker
+                label="Дата отправки"
+                value={data.DateSend}
+                onChange={(newValue: Moment) => {
+                  dispatch(
+                    setSend([
+                      "DateSend",
+                      newValue
+                        ? newValue.isValid()
+                          ? newValue.toISOString()
+                          : newValue.toString()
+                        : "",
+                    ])
+                  );
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    id="date_send"
+                    label="Дата"
+                    {...params}
+                    helperText={errorDateText}
+                    variant="filled"
+                  />
+                )}
               />
-            )}
-          />
-          <TextField
-            error={!data.WhereSend}
-            id="where_send"
-            label="Куда"
-            value={data.WhereSend}
-            onChange={(event) => {
-              dispatch(setSend(["WhereSend", event.target.value]));
-            }}
-            helperText={!data.WhereSend ? "Заполните поле" : " "}
-            variant="outlined"
-          />
-          <Button
-            onClick={SendData}
-            color="primary"
-            variant="contained"
-            sx={{ float: "right", mr: "13px" }}
-          >
-            Отправить
-          </Button>
-        </Box>
-      </Paper>
-    </React.Fragment>
+              <TextField
+                error={!data.WhereSend}
+                id="where_send"
+                label="Куда"
+                value={data.WhereSend}
+                onChange={(event) => {
+                  dispatch(setSend(["WhereSend", event.target.value]));
+                }}
+                helperText={!data.WhereSend ? "Заполните поле" : " "}
+                variant="outlined"
+              />
+              <Button
+                onClick={() => {
+                  SendData();
+                  setShow(false);
+                }}
+                color="primary"
+                variant="contained"
+                sx={{ float: "right", mr: "13px" }}
+              >
+                Отправить
+              </Button>
+            </Box>
+          </Paper>
+        </React.Fragment>
+      ) : null}
+    </>
   );
 }
