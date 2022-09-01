9cb1c42284d8140c231e5425bc83382f5f224370
Ivanova Maria <ivanova_ms@outlook.com>
2022-09-01 17:59:22 +0300

    обновлены пакеты, убран лишний код

--- renderer/components/Admin/OpenAdminPage.tsx
+++ renderer/components/Admin/OpenAdminPage.tsx
@@ -1,19 +1,14 @@
-import { Button, Menu, MenuItem } from '@mui/material';
+import { Button } from '@mui/material';
 import React from 'react';
 import Link from '../Link';
 
 export default function OpenAdminPage() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
   return (
     <>
       <Button
         id="basic-button"
         color="secondary"
         sx={{ width: '180px' }}
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
         variant="contained"
         component={Link}
         href="../RolesData"
--- renderer/components/Docs/updateDocs.tsx
+++ renderer/components/Docs/updateDocs.tsx
@@ -3,18 +3,12 @@ import React from 'react';
 import Link from '../Link';
 
 export default function UpdateDocs() {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-
   return (
     <>
       <Button
         id="basic-button"
         color="secondary"
         sx={{ width: '180px' }}
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
         variant="contained"
         component={Link}
         href="../GridData"
