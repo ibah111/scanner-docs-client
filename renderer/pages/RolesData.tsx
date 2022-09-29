1fdea40638352a6e181a2d9744d73d738be6f99d
Ivanova Maria <ivanova_ms@outlook.com>
2022-09-29 10:10:20 +0300

    данные таблиц перенесены сюда

--- renderer/pages/RolesData.tsx
+++ renderer/pages/RolesData.tsx
@@ -1,9 +1,67 @@
-import Role from '../components/Admin/Role';
+import { Grid } from '@mui/material';
+import {
+  DataGridPremium,
+  GridColumns,
+  GridToolbar,
+} from '@mui/x-data-grid-premium';
+import React from 'react';
+import getRoles, { ResultRole, User } from '../api/getRoles';
+import RoleEdit from '../components/Admin/RoleEdit';
 
-export default function RolesData() {
+export default function Role() {
+  const [rights, setRights] = React.useState<ResultRole>({
+    roles: [],
+    users: [],
+  });
+  const refresh = () => {
+    getRoles().then((res) => {
+      setRights(res);
+    });
+  };
+  const columns: GridColumns<User> = [
+    { field: 'id' },
+    { field: 'login', headerName: 'Логин', width: 250 },
+    {
+      field: 'roles',
+      headerName: 'Роли',
+      width: 250,
+      valueGetter(params) {
+        return params.row.Users_Roles.map((value) => value.Role.title).join(
+          ', ',
+        );
+      },
+    },
+    {
+      field: 'actions',
+      headerName: 'Действия',
+      type: 'actions',
+      getActions(params) {
+        return [
+          <RoleEdit
+            key={0}
+            data={params.row}
+            roles={rights.roles}
+            refresh={refresh}
+          />,
+        ];
+      },
+    },
+  ];
+  React.useEffect(() => {
+    refresh();
+  }, []);
   return (
     <>
-      <Role />
+      <Grid item xs sx={{ width: '100vw', height: '100%' }}>
+        <DataGridPremium
+          sx={{ pl: 2 }}
+          columns={columns}
+          rows={rights.users}
+          components={{
+            Toolbar: GridToolbar,
+          }}
+        />
+      </Grid>
     </>
   );
 }
--- renderer/pages/RowsBoxData.tsx
+++ renderer/pages/RowsBoxData.tsx
@@ -1,8 +1,94 @@
-import TableCodes from '../components/TableCodes/TableCodes';
-export default function RowsBoxData() {
+import { Button, Grid } from '@mui/material';
+import {
+  DataGridPremium,
+  GridFilterModel,
+  GridSortModel,
+} from '@mui/x-data-grid-premium';
+import React from 'react';
+import createCode from '../api/createCode';
+import openRowsBox from '../api/openRowsBox';
+import columns from '../components/TableCodes/columns';
+import { socketConnect } from '../lib/socket';
+import { useAppDispatch, useAppSelector } from '../Reducer';
+import { setBox } from '../Reducer/Box';
+import { setRowDoc } from '../Reducer/RowDoc';
+import { setRowsBox } from '../Reducer/RowsBox';
+
+export default function TableCodes() {
+  const data = useAppSelector((state) => state.RowDoc);
+  const { filterModel, page, pageSize, sortModel } = useAppSelector(
+    (state) => state.RowsBox,
+  );
+  const dispatch = useAppDispatch();
+  const onFilterChange = (filter: GridFilterModel) => {
+    dispatch(setRowsBox(['filterModel', filter]));
+  };
+  const handleSortModelChange = (sort: GridSortModel) => {
+    dispatch(setRowsBox(['sortModel', sort]));
+  };
+  const handlePageChange = (newPage: number) => {
+    dispatch(setRowsBox(['page', newPage]));
+  };
+
+  const handlePageSizeChange = (newPageSize: number) => {
+    dispatch(setRowsBox(['pageSize', newPageSize]));
+  };
+
+  React.useEffect(() => {
+    if (page || pageSize || filterModel || sortModel) {
+      openRowsBox().then((res) => {
+        dispatch(setRowDoc(res));
+      });
+    }
+  }, [page, pageSize, filterModel, sortModel]);
+
+  React.useEffect(() => {
+    const socket = socketConnect('listen-box');
+    socket.on('connect', () => {
+      socket.emit('listen-box');
+    });
+    socket.on('add-item', () => {
+      openRowsBox().then((res) => {
+        dispatch(setRowDoc(res));
+      });
+    });
+  }, []);
   return (
     <>
-      <TableCodes />
+      <Grid item xs container direction="column" alignItems="center">
+        <DataGridPremium
+          sx={{ pl: 2, height: '100%', width: '100%' }}
+          columns={columns}
+          rows={data.rows}
+          paginationMode="server"
+          pagination
+          page={page}
+          onPageChange={handlePageChange}
+          rowCount={data.count}
+          filterMode="server"
+          onFilterModelChange={onFilterChange}
+          filterModel={filterModel}
+          sortModel={sortModel}
+          sortingMode="server"
+          onSortModelChange={handleSortModelChange}
+          pageSize={pageSize}
+          onPageSizeChange={handlePageSizeChange}
+        />
+      </Grid>
+
+      {data.rows.length > 0 && (
+        <Button
+          onClick={() => {
+            dispatch(setBox(['create', true]));
+            createCode();
+          }}
+          color="primary"
+          variant="contained"
+          sx={{ ml: '88vw', mt: '10px', mb: '10px' }}
+        >
+          Создать короб
+        </Button>
+      )}
     </>
   );
 }
