cd99160f54e38d01e26d30221742a0322440ca5d
Alex Dev Sm <leshasmlesha@gmail.com>
2023-11-07 16:03:27 +0300

    feat(token): исправил получение токена

--- main/singleEvents.ts
+++ main/singleEvents.ts
@@ -3,26 +3,41 @@ import open from 'open';
 import http from 'http';
 import { createHttpTerminator } from 'http-terminator';
 import url from 'url';
+const headers = {
+  'Access-Control-Allow-Origin': '*' /* @dev First, read about security */,
+  'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+  'Access-Control-Max-Age': 2592000, // 30 days
+  /** add other headers as per requirement */
+};
 export const singleEvents = (webContents: WebContents) => {
   ipcMain.on('OpenInBrowser', (event, value: string) => {
     open(value);
     let error = true;
     const server = http.createServer(async (req, res) => {
-      const query = url.parse(req.url, true).query;
-      webContents.send('getToken', query.token);
-      res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8',
-      });
-      res.end('Вход успешно выполнен');
-      error = false;
-      await httpTerminator.terminate();
+      if (req.method === 'OPTIONS') {
+        res.writeHead(204, headers);
+        res.end();
+        return;
+      }
+      if (['GET', 'POST'].indexOf(req.method) > -1) {
+        const query = url.parse(req.url, true).query;
+        if (query.token) {
+          webContents.send('getToken', query.token);
+          res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=utf-8',
+          });
+          res.end('Вход успешно выполнен');
+          error = false;
+          await httpTerminator.terminate();
+        } else {
+          res.writeHead(200, headers);
+          res.end('Ok');
+        }
+      }
     });
     const httpTerminator = createHttpTerminator({
       server,
     });
-    setTimeout(async () => {
-      if (error) await httpTerminator.terminate();
-    }, 10000);
     server.listen(11712);
   });
 };
