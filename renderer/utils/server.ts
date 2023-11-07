936495ea1d6dbb392aedd8ec706cd0c1efcab214
Alex Dev Sm <leshasmlesha@gmail.com>
2023-11-07 14:55:05 +0300

    feat(config): переписал конфиг

--- renderer/utils/server.ts
+++ renderer/utils/server.ts
@@ -1,16 +1,4 @@
 import config from '../config/server.json';
-export default function server() {
-  let prot;
-  switch (config.protocol) {
-    case 'http':
-      prot = 'http';
-      break;
-    case 'ssl':
-      prot = 'https';
-      break;
-    default:
-      prot = 'http';
-      break;
-  }
-  return `${prot}://${config.server}:${config.port}`;
+export default function server(name?: string) {
+  return config.server;
 }
