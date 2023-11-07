0afe6f80019646b1e51cd689eeea5b8d474b4360
Alex Dev Sm <leshasmlesha@gmail.com>
2023-11-07 17:33:13 +0300

    fix(roles): исправил роли

--- renderer/components/Login.tsx
+++ renderer/components/Login.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch, useAppSelector } from '../Reducer';
 import { resetLogin, setState } from '../Reducer/State';
 import Authorization from './MainPage/Authorization';
 import getToken from '../api/getToken';
+import { setUser } from '../Reducer/User';
 
 interface LoginProps {
   children: React.ReactNode;
@@ -18,7 +19,11 @@ export default function Login({ children }: LoginProps) {
     if (reload) {
       getToken().subscribe({
         next: (data) => {
+          dispatch(setUser(data));
           setLoged(data.login_result);
+        },
+        error: () => {
+          dispatch(setUser(null));
         },
         complete: () => {
           dispatch(setState(['login', false]));
