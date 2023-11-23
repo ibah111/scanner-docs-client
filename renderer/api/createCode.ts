9b8adeb3c965f2c33649317dcd1b77ea2a47c891
Ibah <starlord77@mail.ru>
2023-11-23 14:32:09 +0300

    feat(bpac): написал проверку и занес под observable

--- renderer/api/createCode.ts
+++ renderer/api/createCode.ts
@@ -1,45 +1,27 @@
-import bpac_electron from '@tools/bpac';
-import axios from 'axios';
-import printBarcode from '../lib/printBarcode';
 import { store } from '../Reducer';
-import { callError, callSuccess } from '../Reducer/Message';
-import { resetRowDoc } from '../Reducer/RowDoc';
 import { DocData } from '../Schemas/DocData.model';
 import { forkJoin, lastValueFrom, of } from 'rxjs';
 import { baseRequest } from '../utils/baseRequest';
 import { authRetry, post, transformAxios } from '@tools/rxjs-pipes';
 import { transformError } from '../utils/processError';
-
+import isPrintOnline from '../lib/isPrintOnline';
 const url = of('/createBox');
+
 export default async function createCode() {
   const data = store.getState().Box;
-  const bpac = bpac_electron();
-  const doc = bpac.IDocument;
-  const printerName = await doc.GetPrinterName();
-  const printer = new bpac.IPrinter('');
-  const is_online = await printer.IsPrinterOnline(printerName);
-  if (printerName == 'Brother QL-800' && is_online) {
-    return lastValueFrom(
-      forkJoin([
-        baseRequest,
-        url,
-        of({
-          ...data,
-        }),
-      ]).pipe(post<DocData>(), transformAxios(), transformError(), authRetry()),
-    )
-      .then((res) => {
-        printBarcode(String(res));
-        store.dispatch(callSuccess('Штрих-код успешно создан'));
-        store.dispatch(resetRowDoc());
-      })
-      .catch((e) => {
-        if (axios.isAxiosError(e)) {
-          store.dispatch(callError(e.response.data.message));
-        }
-        throw e;
-      });
-  } else {
-    store.dispatch(callError('Подключите принтер Brother QL-800'));
-  }
+  return lastValueFrom(
+    forkJoin([
+      baseRequest,
+      url,
+      of({
+        ...data,
+      }),
+    ]).pipe(
+      isPrintOnline(),
+      post<DocData>(),
+      transformAxios(),
+      transformError(),
+      authRetry(),
+    ),
+  );
 }
---
+++ renderer/lib/isPrintOnline.ts
@@ -0,0 +1,26 @@
+import { bpac_electron } from '@tools/bpac/electron_renderer';
+import { from, map, mergeMap, of } from 'rxjs';
+const printers = ['Brother QL-800'];
+export default function isPrintOnline<T>() {
+  return mergeMap((value: T) =>
+    of(value).pipe(
+      () => {
+        const bpac = bpac_electron();
+        const doc = bpac.IDocument;
+        return from(doc.GetPrinterName()).pipe(
+          mergeMap((printerName) => {
+            const printer = new bpac.IPrinter('');
+            return from(printer.IsPrinterOnline(printerName)).pipe(
+              map((isOnline) => ({ isOnline, printerName })),
+            );
+          }),
+        );
+      },
+      map((data) => {
+        if (!(data.isOnline && printers.includes(data.printerName)))
+          throw Error('Ошибка принтер недоступен');
+      }),
+      map(() => value),
+    ),
+  );
+}
