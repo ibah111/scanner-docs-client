83d8aa463571284b50129aa59075fd0939a60372
Ibah <starlord77@mail.ru>
2023-11-23 11:36:45 +0300

    feat(bpac): переместил bpac/electon в preload

--- main/preload.ts
+++ main/preload.ts
@@ -1,13 +1,18 @@
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 import type Conf from 'conf';
+import '@electron/remote';
+
 const ipcHandler = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   send(channel: string, ...args: any[]) {
     ipcRenderer.send(channel, ...args);
   },
   on(
     channel: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     callback: (_event: IpcRendererEvent, ...args: any[]) => void,
   ) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const subscription = (_event: IpcRendererEvent, ...args: any[]) =>
       callback(_event, ...args);
     ipcRenderer.on(channel, subscription);
@@ -18,8 +23,10 @@ const ipcHandler = {
   },
   once(
     channel: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     callback: (_event: IpcRendererEvent, ...args: any[]) => void,
   ) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const subscription = (_event: IpcRendererEvent, ...args: any[]) =>
       callback(_event, ...args);
     ipcRenderer.once(channel, subscription);
