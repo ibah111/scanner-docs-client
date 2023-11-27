8ddb40fa5edc2889c7b6355a62eefc52be6d6f99
Ibah <starlord77@mail.ru>
2023-11-27 12:03:00 +0300

    fix(admin-table): пофиксил отображение таблцы (не работает пагинация)

--- renderer/pages/AdminPage/index.tsx
+++ renderer/pages/AdminPage/index.tsx
@@ -1,13 +1,28 @@
 import { Grid } from '@mui/material';
 import { DataGridPremium } from '@mui/x-data-grid-premium';
 import useAdminPageGrid from './useAdminPageGrid';
+import { CustomToolbar } from '../../components/CustomToolbar';
+import CustomPagination from '../../components/Pagination/CustomPagination';
 
 export default function AmdinPage() {
   const { ...gridProps } = useAdminPageGrid();
   return (
     <>
-      <Grid item xs sx={{ width: '100vw', height: '100%' }}>
-        <DataGridPremium {...gridProps} sx={{ pl: 2 }} />
+      <Grid
+        item
+        xs
+        sx={{ width: '100%', height: 520 }}
+        container
+        direction={'column'}
+      >
+        <DataGridPremium
+          {...gridProps}
+          sx={{ pl: 2 }}
+          slots={{
+            toolbar: CustomToolbar,
+            pagination: CustomPagination,
+          }}
+        />
       </Grid>
     </>
   );
