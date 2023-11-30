0268edcc16dfa7c94113df635fb7c500810fe651
Ibah <starlord77@mail.ru>
2023-11-30 10:36:20 +0300

    feat(doc-instance-post): поменял класс запроса на instance

--- renderer/api/createDocument.ts
+++ renderer/api/createDocument.ts
@@ -2,13 +2,19 @@ import { forkJoin, lastValueFrom, of } from 'rxjs';
 import { baseRequest } from '../utils/baseRequest';
 import { authRetry, post, transformAxios } from '@tools/rxjs-pipes/axios';
 import { transformError } from '../utils/processError';
-import { Doc } from '../Schemas/Doc.model';
+import { DocInstance } from '../Reducer/Doc';
 
 const url = of('/create');
-export default function createDocument(body: Doc) {
+export default function createDocument(body: DocInstance) {
   return lastValueFrom(
-    forkJoin([baseRequest, url, of({ body })]).pipe(
-      post<Doc>(),
+    forkJoin([
+      baseRequest,
+      url,
+      of({
+        ...body,
+      }),
+    ]).pipe(
+      post<DocInstance>(),
       transformAxios(),
       transformError(),
       authRetry(),
