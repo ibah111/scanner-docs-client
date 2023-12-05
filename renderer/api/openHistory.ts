af4e7cf37bcd823918854fdc7f1c05efa2630d89
Ibah <starlord77@mail.ru>
2023-12-05 14:37:37 +0300

    feat(open-history-api): переписал api получения истории документа

--- renderer/api/openHistory.ts
+++ renderer/api/openHistory.ts
@@ -1,16 +1,22 @@
 import { Log } from '../Schemas/Log.model';
 import { forkJoin, lastValueFrom, of } from 'rxjs';
 import { baseRequest } from '../utils/baseRequest';
-import { authRetry, post, transformAxios } from '@tools/rxjs-pipes';
+import {
+  authRetry,
+  get,
+  transformAxios,
+  createFormat,
+} from '@tools/rxjs-pipes';
 import { transformError } from '../utils/processError';
 /**
  * @todo
  */
-const url = of('/openHistory');
+const url = of('/openHistory/%1$d');
+const format = createFormat<[number]>();
 export default async function openHistory(code: number) {
   return lastValueFrom(
-    forkJoin([baseRequest, url, of({ code })]).pipe(
-      post<Log[]>(),
+    forkJoin([baseRequest, url.pipe(format(code))]).pipe(
+      get<Log[]>(),
       transformAxios(),
       transformError(),
       authRetry(),
