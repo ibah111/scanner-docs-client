bdb678e6345c6952a7b7b27a13426d45a826d54d
Ibah <starlord77@mail.ru>
2023-12-05 12:06:40 +0300

    feat(send-reducer): редюсер отправки

--- renderer/Reducer/Send.ts
+++ renderer/Reducer/Send.ts
@@ -1,26 +1,51 @@
 import { createSlice, Draft, PayloadAction } from '@reduxjs/toolkit';
+import { IsValidMoment } from '../hooks/Validation/IsValidMoment';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { DateType } from '../hooks/Validation/DateType';
+import { TransformDate } from '../hooks/Validation/TransformDate';
+import { Model } from '@sql-tools/sequelize-typescript';
+import { CreateLiteralAssociation } from '@sql-tools/association-literal';
+import { InferAttributes, InferCreationAttributes } from '@sql-tools/sequelize';
+import { CreationAttributes } from '@sql-tools/sequelize';
 
-interface SendData {
-  DateSend: string;
+export class Send extends Model<
+  InferAttributes<Send>,
+  InferCreationAttributes<Send>,
+  CreateLiteralAssociation<Send>
+> {
+  DataSend: moment.Moment;
   WhereSend: string;
 }
-export const startSendState: SendData = {
-  DateSend: '',
-  WhereSend: '',
-};
-const initialState = startSendState;
+export class SendInstance {
+  @IsValidMoment()
+  @IsNotEmpty()
+  @DateType(false)
+  @TransformDate(false)
+  DateSend: moment.Moment;
+
+  @IsNotEmpty()
+  @IsString()
+  WhereSend: string;
+}
+export type SendData = CreationAttributes<Send>;
+
+const initialState = {} as SendInstance;
+
 const SendSlice = createSlice({
   name: 'Send',
   initialState,
   reducers: {
-    setSend<T extends keyof SendData>(
-      state: Draft<SendData>,
-      action: PayloadAction<[T, SendData[T]]>,
+    setSend(_, action: PayloadAction<SendInstance>) {
+      return action.payload;
+    },
+    setSendProperty<T extends keyof SendInstance>(
+      state: Draft<SendInstance>,
+      action: PayloadAction<[T, SendInstance[T]]>,
     ) {
       state[action.payload[0]] = action.payload[1];
     },
     resetSend() {
-      return startSendState;
+      return initialState;
     },
   },
 });
