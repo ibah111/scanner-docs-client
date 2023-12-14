3960a6bc9bb7b7f1859380af0ef7e78435b92972
Ibah <starlord77@mail.ru>
2023-12-14 16:35:48 +0300

    feat(getCodes-api): апи на получение кодов

---
+++ renderer/api/Codes/getCodes.ts
@@ -0,0 +1,25 @@
+import { forkJoin, lastValueFrom, of } from 'rxjs';
+import { baseRequest } from '../../utils/baseRequest';
+import {
+  authRetry,
+  createFormat,
+  get,
+  transformAxios,
+} from '@tools/rxjs-pipes';
+import { transformError } from '../../utils/processError';
+class getCodesResponse {
+  doc_code: string;
+  box_code: string;
+}
+const url = of('/codes/%1$s');
+const format = createFormat<[string]>();
+export default function getCodes(id: number) {
+  return lastValueFrom(
+    forkJoin([baseRequest, url.pipe(format(String(id)))]).pipe(
+      get<getCodesResponse>(),
+      transformAxios(),
+      transformError(),
+      authRetry(),
+    ),
+  );
+}
