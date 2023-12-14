51083b817cce23274f3d056b5a9b5f1aa7ea5582
Ibah <starlord77@mail.ru>
2023-12-14 16:39:02 +0300

    feat(printDialog): рисую диалог форму на ре-печать

--- renderer/pages/DocumentPage/DocumentComponents/PrintDialog.tsx
+++ renderer/pages/DocumentPage/DocumentComponents/PrintDialog.tsx
@@ -4,32 +4,65 @@ import {
   DialogContent,
   DialogTitle,
   Grid,
+  Tooltip,
 } from '@mui/material';
 import printBarcode from '../../../lib/printBarcode';
+import React from 'react';
 interface PrintDialogInterface {
   open: boolean;
   onClose: VoidFunction;
+  docId: number;
 }
-export default function PrindDialog({ onClose, open }: PrintDialogInterface) {
+export default function PrindDialog({
+  onClose,
+  open,
+  docId,
+}: PrintDialogInterface) {
+  const [boxCode, setBoxCode] = React.useState<number>();
+  const [docCode, setDocCode] = React.useState<number>();
+  const condition = (box: number): boolean => {
+    if (box) return false;
+    return true;
+  };
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Печать кода:</DialogTitle>
+    <Dialog open={open} onClose={onClose} fullWidth maxWidth={'md'}>
+      <DialogTitle align="center">Документ: {docId}, Печать кода:</DialogTitle>
       <DialogContent>
-        <Grid container xs item>
-          <Button
-            onClick={() => {
-              printBarcode(String(0));
-            }}
-          >
-            Короба
-          </Button>
-          <Button
-            onClick={() => {
-              printBarcode(String(0));
-            }}
-          >
-            Документ
-          </Button>
+        <Grid container columnSpacing={2}>
+          <Grid xs item>
+            <Button
+              fullWidth
+              color="success"
+              variant="contained"
+              onClick={() => {
+                printBarcode(String(docCode)).subscribe({
+                  complete: () => {},
+                  error: () => {},
+                  next: () => {},
+                });
+              }}
+            >
+              <Grid>Документ: {docCode}</Grid>
+            </Button>
+          </Grid>
+          <Grid xs item>
+            <Tooltip title="Если кнопка деактивирована, то клиент не получил код короба">
+              <Button
+                disabled={condition}
+                fullWidth
+                variant="contained"
+                onClick={() => {
+                  printBarcode(String(boxCode)).subscribe({
+                    complete: () => {},
+                    error: () => {},
+                    next: () => {},
+                  });
+                }}
+              >
+                <Grid>Короба: {boxCode}</Grid>
+              </Button>
+            </Tooltip>
+          </Grid>
         </Grid>
       </DialogContent>
     </Dialog>
