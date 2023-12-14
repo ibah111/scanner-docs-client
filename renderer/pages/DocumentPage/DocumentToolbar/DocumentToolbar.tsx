994553f9919fab87d0cc9adad7e3f02c787ec28d
Ibah <starlord77@mail.ru>
2023-12-14 17:26:23 +0300

    refactor(addDocButton): вырезал создание документа
    документы добавляются через апи входяшки

--- renderer/pages/DocumentPage/DocumentToolbar/DocumentToolbar.tsx
+++ renderer/pages/DocumentPage/DocumentToolbar/DocumentToolbar.tsx
@@ -4,27 +4,13 @@ import {
   GridToolbarDensitySelector,
   GridToolbarFilterButton,
 } from '@mui/x-data-grid-premium';
-import AddDocumentButton from './AddDocumentButton';
-import { Can } from '../../../casl/casl.factory';
-import { Action, Subject } from '../../../casl/casl';
 
-interface DocToolbarProps {
-  handleOpenDocumentCreate: VoidFunction;
-}
-export function DocumentToolbar({ handleOpenDocumentCreate }: DocToolbarProps) {
+export function DocumentToolbar() {
   return (
     <GridToolbarContainer>
       <GridToolbarColumnsButton />
       <GridToolbarFilterButton />
       <GridToolbarDensitySelector />
-      {/**
-       * @TODO добавить can
-       */}
-      <Can I={Action.Manage} a={Subject.Document}>
-        <AddDocumentButton
-          handleOpenDocumentCreate={handleOpenDocumentCreate}
-        />
-      </Can>
     </GridToolbarContainer>
   );
 }
