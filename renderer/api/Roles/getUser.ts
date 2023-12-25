fa5ebbdb2989b01c577237242dab674f489523b7
Ibah <starlord77@mail.ru>
2023-12-25 17:42:50 +0300

    refactor(getUser-api): поменял апи

--- renderer/api/Roles/getUser.ts
+++ renderer/api/Roles/getUser.ts
@@ -6,16 +6,20 @@ import {
   get,
   transformAxios,
 } from '@tools/rxjs-pipes';
-import { User } from './getRoles';
+import { Role, User } from './getRoles';
 import { transformError } from '../../utils/processError';
 
-const url = of('getUser/%1$s');
+class AdminRole {
+  user: User;
+  notInRoles: Role[];
+}
+
 const url = of('role/getUser/%1$s');
 const format = createFormat<[string]>();
 export default function getUser(id: number) {
   return lastValueFrom(
     forkJoin([baseRequest, url.pipe(format(String(id)))]).pipe(
-      get<User>(),
+      get<AdminRole>(),
       transformAxios(),
       transformError(),
       authRetry(),
