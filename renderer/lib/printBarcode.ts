e2f1eb27883e5d885bb720c1255786bc9b13b78d
Ibah <starlord77@mail.ru>
2024-02-05 11:00:06 +0300

    feat(new-maket): новый макет штрихкода
    Добавил возможность вывода любого текста

--- renderer/lib/printBarcode.ts
+++ renderer/lib/printBarcode.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { bpac_electron } from '@tools/bpac/electron_renderer';
 import { from, mergeMap, of, tap } from 'rxjs';
 import isPrintOnline from './isPrintOnline';
-export default function printBarcode(text: string) {
+export default function printBarcode(text: string, title: string) {
   const bpac = bpac_electron();
   return of(text).pipe(
     isPrintOnline(),
@@ -11,12 +11,16 @@ export default function printBarcode(text: string) {
       const barcode_file = path.join(
         window.getCwd(),
         'extensions',
-        'barcode.lbx',
+        'test-barcode.lbx',
       );
       return from(doc.Open(barcode_file)).pipe(
-        mergeMap(() => doc.GetObject('barcode')),
-        tap((barcode) => {
-          barcode.Text = String(text);
+        mergeMap(() => doc.GetObject('text')),
+        tap((lbx) => {
+          // Наименование к баркоду
+          lbx.Text = String(title);
+          // Даю значение к штрихкоду
+          doc.SetBarcodeData(0, String(text));
+          // Принт функции
           doc.StartPrint('', 0);
           doc.PrintOut(1, 0);
           doc.EndPrint();
