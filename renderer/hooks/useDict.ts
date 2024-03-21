0e61a6844603115b83eab733bfcf2bbc882b51da
Ibah <starlord77@mail.ru>
2024-03-21 12:00:47 +0300

    feat: хук словарей

---
+++ renderer/hooks/useDict.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import getDict from '../apiSend/getDict';
+import { useAppDispatch, useAppSelector } from '../ReducerSend';
+import { setDict } from '../ReducerSend/Dict';
+
+export default function useDict(id: number) {
+  const dict = useAppSelector((state) => state.Dict[id]);
+  const dispatch = useAppDispatch();
+  React.useEffect(() => {
+    if (!dict) {
+      const data = getDict(id).subscribe((res) => dispatch(setDict(res)));
+      return () => {
+        data.unsubscribe();
+      };
+    }
+  }, [dict, dispatch, id]);
+  return dict || [];
+}
