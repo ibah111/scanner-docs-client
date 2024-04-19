fd28dcddbc82629f10bfa3f78b1fb247ad56ac68
Ibah <starlord77@mail.ru>
2024-04-19 12:27:11 +0300

    feat: delete auto update events

--- main/autoUpdaters.ts
+++ main/autoUpdaters.ts
@@ -4,24 +4,6 @@ import { LastAvailableVersion } from './utils';
 
 export default function autoUpdaters(app: App, webContents: WebContents) {
   autoUpdater.autoDownload = false;
-  ipcMain.on('update-download', () => {
-    autoUpdater.downloadUpdate();
-  });
-  ipcMain.on('update-install', () => {
-    autoUpdater.quitAndInstall();
-  });
-  autoUpdater.on('update-available', (info) => {
-    webContents.send('update-available', info);
-  });
-  autoUpdater.on('error', (err) => {
-    webContents.send('message-error', err);
-  });
-  autoUpdater.on('download-progress', (progressObj) => {
-    webContents.send('download-progress', progressObj.percent);
-  });
-  autoUpdater.on('update-downloaded', () => {
-    webContents.send('update-downloaded');
-  });
 
   ipcMain.on('check_version', async function () {
     const getVersion = (): string => {
