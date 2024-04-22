75094cb254391d298139ceb0d17f82eb58488350
Ibah <starlord77@mail.ru>
2024-04-22 16:46:09 +0300

    fix(autoUpdate): not in time called functions

--- main/autoUpdaters.ts
+++ main/autoUpdaters.ts
@@ -1,14 +1,9 @@
 import { App, ipcMain, WebContents } from 'electron';
 import { autoUpdater } from 'electron-updater';
-import { LastAvailableVersion } from './utils';
 
 export default function autoUpdaters(app: App, webContents: WebContents) {
   autoUpdater.autoDownload = false;
 
-  autoUpdater.checkForUpdates().then((res) => {
-    console.log('check for updates', res.updateInfo);
-    return res;
-  });
   autoUpdater.on('error', (err) => {
     webContents.send('message-error', err);
   });
@@ -25,16 +20,6 @@ export default function autoUpdaters(app: App, webContents: WebContents) {
     webContents.send('update-downloaded');
   });
   ipcMain.on('check_version', async function () {
-    const getVersion = (): string => {
-      const version = app.getVersion();
-      return version;
-    };
-    const currentVersion = getVersion();
-
-    console.log(
-      'check_version',
-      `currentVersion ${currentVersion} // last available ${LastAvailableVersion}`,
-    );
     autoUpdater.checkForUpdates().then((res) => {
       console.log('results', res);
       return res.updateInfo;
