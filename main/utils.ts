90358fee79b7f496c9d1e927711587ea29558139
Ibah <starlord77@mail.ru>
2024-04-22 16:45:24 +0300

    chore(main-server): deprecated func

--- main/utils.ts
+++ main/utils.ts
@@ -1,5 +1,8 @@
 import gitSemverTags from 'git-semver-tags';
-
+/**
+ * never used, but i will keep it
+ * @deprecated
+ */
 export const LastAvailableVersion = await gitSemverTags().then(
   (tags) => tags[0],
 );
