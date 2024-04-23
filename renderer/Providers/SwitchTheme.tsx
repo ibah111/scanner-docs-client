ceaa9651a3f0e7161e80f82a795829e889c8665d
Ibah <starlord77@mail.ru>
2024-04-23 17:02:11 +0300

    feat(theme): switch theme provider

---
+++ renderer/Providers/SwitchTheme.tsx
@@ -0,0 +1,22 @@
+import LightModeIcon from '@mui/icons-material/LightMode';
+import { Button } from '@mui/material';
+import React from 'react';
+import { ColorModeContext } from './ThemeProvider';
+export default function SwitchTheme() {
+  const { toggleColorMode } = React.useContext(ColorModeContext);
+  console.log('Mode', ColorModeContext.displayName);
+  return (
+    <>
+      <Button
+        color="inherit"
+        onClick={() => toggleColorMode()}
+        id="basic-button"
+        sx={{
+          mt: 0.5,
+        }}
+      >
+        <LightModeIcon />
+      </Button>
+    </>
+  );
+}
