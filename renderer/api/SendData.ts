f75234b3668e50d7a0f7b63e7b300dc6e680c4c0
Ibah <starlord77@mail.ru>
2024-05-02 14:47:43 +0300

    chore(sendData): changed sendData name variable

--- renderer/api/SendData.ts
+++ renderer/api/SendData.ts
@@ -6,7 +6,7 @@ import { authRetry, post, transformAxios } from '@tools/rxjs-pipes';
 import { transformError } from '../utils/processError';
 
 export default async function SendData() {
-  const data = store.getState().Send;
+  const scannerSendData = store.getState().SendDoc;
   const id = store.getState().DocArray[0].id;
 
   const url = of('/send');
@@ -15,8 +15,8 @@ export default async function SendData() {
       baseRequest,
       url,
       of({
-        ...data,
         id,
+        ...scannerSendData,
       }),
     ]).pipe(post<Transmit>(), transformAxios(), transformError(), authRetry()),
   );
