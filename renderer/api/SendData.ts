449123d90cadc8b9a86ccaf932794ca7c3161f89
Ibah <starlord77@mail.ru>
2024-05-03 18:06:39 +0300

    feat(scanner-send-api): scanner send api

--- renderer/api/SendData.ts
+++ renderer/api/SendData.ts
@@ -4,19 +4,23 @@ import { forkJoin, lastValueFrom, of } from 'rxjs';
 import { baseRequest } from '../utils/baseRequest';
 import { authRetry, post, transformAxios } from '@tools/rxjs-pipes';
 import { transformError } from '../utils/processError';
+import moment from 'moment';
 
-export default async function SendData() {
-  const scannerSendData = store.getState().SendDoc;
+export default async function SendData(data: {
+  WhereSend: string;
+  DateSend: moment.Moment;
+}) {
   const id = store.getState().DocArray[0].id;
-
   const url = of('/send');
+  console.log('SendData: => ', data);
   return lastValueFrom(
     forkJoin([
       baseRequest,
       url,
       of({
         id,
-        ...scannerSendData,
+        DateSend: data.DateSend,
+        WhereSend: data.WhereSend,
       }),
     ]).pipe(post<Transmit>(), transformAxios(), transformError(), authRetry()),
   );
