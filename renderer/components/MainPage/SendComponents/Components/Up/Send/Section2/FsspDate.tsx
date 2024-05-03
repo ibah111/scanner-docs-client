6b565cf4430a9abe5d38876629704dd8c8433d7e
Ibah <starlord77@mail.ru>
2024-05-03 18:08:05 +0300

    feat(scanner-send): date send equal fssp date

--- renderer/components/MainPage/SendComponents/Components/Up/Send/Section2/FsspDate.tsx
+++ renderer/components/MainPage/SendComponents/Components/Up/Send/Section2/FsspDate.tsx
@@ -3,17 +3,25 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import getData from '../../../../../../../utils/getData';
+import { useAppDispatch, useAppSelector } from '../../../../../../../Reducer';
+import { setSendDocProperty } from '../../../../../../../Reducer/SendDoc';
+import moment from 'moment';
 
 export default function FsspDate() {
   const { t } = useTranslation();
   const data = getData('fssp_date', 'date');
+  const dispatch = useAppDispatch();
+  const DateSend = useAppSelector((state) => state.SendDoc.DateSend);
   return (
     <>
       <Grid item>
         <DatePicker
           label={t('form.send.fssp_date')}
           value={data.value}
-          onChange={(newValue) => data.setValue(newValue)}
+          onChange={(newValue) => {
+            data.setValue(newValue);
+            dispatch(setSendDocProperty(['DateSend', moment(newValue)]));
+          }}
           slotProps={{
             textField: {
               fullWidth: true,
