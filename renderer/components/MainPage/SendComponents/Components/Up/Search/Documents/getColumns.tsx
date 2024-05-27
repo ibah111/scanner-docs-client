dc396758966a1c2c15b782340452f1d219525609
Ibah <starlord77@mail.ru>
2024-05-27 17:13:21 +0300

    feat: two array of columns

--- renderer/components/MainPage/SendComponents/Components/Up/Search/Documents/getColumns.tsx
+++ renderer/components/MainPage/SendComponents/Components/Up/Search/Documents/getColumns.tsx
@@ -3,8 +3,9 @@ import { GridColDef, GridValueGetterParams } from '@mui/x-data-grid-premium';
 import { t } from 'i18next';
 import { generateName } from '../../../../../../../utils/generateName';
 import Actions from './Actions';
+import moment from 'moment';
 
-export default function getColumns(refresh: () => void) {
+export function getLawExecColumns(refresh: () => void) {
   const columns: GridColDef<DocAttach>[] = [
     { field: 'id', headerName: 'ID' },
     {
@@ -33,4 +34,46 @@ export default function getColumns(refresh: () => void) {
     },
   ];
   return columns;
+}
+
+export function getLawActColumns(refresh: () => void) {
+  const columns: GridColDef<DocAttach>[] = [
+    {
+      field: 'id',
+      headerName: 'ID судебной работы',
+    },
+    {
+      field: 'name',
+      headerName: 'Наименование вложения',
+      type: 'string',
+    },
+    {
+      field: 'dt',
+      type: 'date',
+      headerName: 'Дата документа',
+      valueGetter(params) {
+        return moment(params.row.dt).toDate();
+      },
+    },
+    {
+      field: 'User',
+      headerName: t('form.documents.table.user'),
+      valueGetter: (params: GridValueGetterParams<DocAttach, User>) =>
+        generateName(params.value?.f, params.value?.i, params.value?.o),
+    },
+    {
+      field: 'actions',
+      type: 'actions',
+      headerName: t('form.documents.table.actions.name'),
+      getActions: (params) => [
+        <Actions key={1} id={params.row.id} refresh={refresh} />,
+      ],
+    },
+  ];
+  return columns.map<GridColDef<DocAttach>>((items) => ({
+    width: 100,
+    align: 'center',
+    headerAlign: 'center',
+    ...items,
+  }));
 }
