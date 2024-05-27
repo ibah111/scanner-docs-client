90c3ea51721a2caebbee5a14b9022c995644f89a
Ibah <starlord77@mail.ru>
2024-05-27 17:12:32 +0300

    feat(api): fixed api added a new one

---
+++ renderer/apiSend/Documents/getLawActDocuments.ts
@@ -0,0 +1,15 @@
+import { DocAttach } from '@contact/models';
+import { forkJoin, of } from 'rxjs';
+import { post, transformAxios, authRetry } from '@tools/rxjs-pipes/axios';
+import { transformError } from '../../utils/processError';
+import { sendApiRequestInstanceObservable } from '../../utils/sendUtils/requests';
+const url = of('/documents/getLawAct');
+export default function getLawActDocuments(law_act_id: number) {
+  return forkJoin([
+    sendApiRequestInstanceObservable,
+    url,
+    of({
+      law_act_id,
+    }),
+  ]).pipe(post<DocAttach[]>(), transformAxios(), transformError(), authRetry());
+}
