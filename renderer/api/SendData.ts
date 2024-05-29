92c4f6139e327888906b33abd239d7f57e03af95
Ibah <starlord77@mail.ru>
2024-05-29 11:57:55 +0300

    feat: changed submit button and input send data api variables

--- renderer/api/SendData.ts
+++ renderer/api/SendData.ts
@@ -1,4 +1,3 @@
-import { store } from '../Reducer';
 import { Transmit } from '../Schemas/Transmit.model';
 import { forkJoin, lastValueFrom, of } from 'rxjs';
 import { baseRequest } from '../utils/baseRequest';
@@ -7,17 +6,17 @@ import { transformError } from '../utils/processError';
 import moment from 'moment';
 
 export default async function SendData(data: {
+  id: number;
   WhereSend: string;
   DateSend: moment.Moment;
 }) {
-  const id = store.getState().DocArray[0].id;
   const url = of('/send');
   return lastValueFrom(
     forkJoin([
       baseRequest,
       url,
       of({
-        id,
+        id: data.id,
         DateSend: data.DateSend,
         WhereSend: data.WhereSend,
       }),
--- renderer/components/MainPage/SendComponents/Components/Up/Search/Submit.tsx
+++ renderer/components/MainPage/SendComponents/Components/Up/Search/Submit.tsx
@@ -3,7 +3,11 @@ import { t } from 'i18next';
 import { useSnackbar, VariantType } from 'notistack';
 import React from 'react';
 import updateExec from '../../../../../../apiSend/Exec/updateExec';
-import { useAppDispatch, useAppSelector } from '../../../../../../Reducer';
+import {
+  store,
+  useAppDispatch,
+  useAppSelector,
+} from '../../../../../../Reducer';
 import { saveAs } from 'file-saver';
 import { LoadingButton } from '@mui/lab';
 import { useTranslation } from 'react-i18next';
@@ -16,7 +20,6 @@ import moment from 'moment';
  * default export useError as getData
  */
 import getData from '../../../../../../utils/getData';
-import getCourt from '../../../../../../apiSend/Court/getCourt';
 
 function toArrayBuffer(buf: number[]) {
   const ab = new ArrayBuffer(buf.length);
@@ -57,49 +60,73 @@ export default function Submit() {
     [enqueueSnackbar],
   );
   const date_send = getData('fssp_date', 'date');
-  const r_court_id = getData('r_court_id', 'null');
+  const where = getData('r_court_id', 'null');
+
+  const state_id = store.getState().DocArray[0]?.id;
+  const WhereSend = where.value as string;
+  const DateSend = moment(date_send.value);
+
+  //Tracking document callback
+  const SendTrackingDocument = React.useCallback(async () => {
+    return SendData({
+      id: state_id,
+      DateSend,
+      WhereSend,
+    });
+  }, []);
+
+  //LawExec callback
+  const UpdateLawExec = React.useCallback(
+    () =>
+      updateExec().subscribe({
+        next: (res) => {
+          if (res) {
+            const file = new Blob([toArrayBuffer(res.file.data)], {
+              type: 'application/pdf',
+            });
+            saveAs(file, res.name);
+            ResetSendData();
+            setLoading(false);
+          } else {
+            setLoading(false);
+            if (res === null) {
+              dispatch(callError('Дело не имеет изменений'));
+            }
+            if (res === false) {
+              dispatch(callError('Вас нет в Контакте, обратитесь в IT отдел'));
+            }
+          }
+        },
+        error: () => setLoading(false),
+        complete: () => setLoading(false),
+      }),
+    [],
+  );
+
+  //Click callback function
   const Click = React.useCallback(() => {
     if (check(Error, AddAlert)) {
       setLoading(true);
-      getCourt({ id: r_court_id.value as number }).subscribe((court) => {
-        const o = court[0];
-        const where_send_court = `(${o.id}), ${o.name}, ${o.address}, ${o.district}`;
-        SendData({
-          DateSend: moment(date_send.value),
-          WhereSend: where_send_court,
-        }).then((res) => {
-          enqueueSnackbar('Отправлено!', {
-            variant: 'success',
-          });
+      //
+      if (state_id && DateSend && WhereSend) {
+        enqueueSnackbar('Отправляю отслеживаемый документ', {
+          variant: 'info',
+        });
+        SendTrackingDocument().then((res) => {
           if (res) {
-            updateExec().subscribe({
-              next: (res) => {
-                if (res) {
-                  const file = new Blob([toArrayBuffer(res.file.data)], {
-                    type: 'application/pdf',
-                  });
-                  saveAs(file, res.name);
-                  ResetSendData();
-                  setLoading(false);
-                } else {
-                  setLoading(false);
-                  if (res === null) {
-                    dispatch(callError('Дело не имеет изменений'));
-                  }
-                  if (res === false) {
-                    dispatch(
-                      callError('Вас нет в Контакте, обратитесь в IT отдел'),
-                    );
-                  }
-                }
-              },
-              error: () => setLoading(false),
-            });
+            console.log('Sended tracking document, Transmit Results: ', res);
+            UpdateLawExec();
           }
         });
+      }
+      //
+      enqueueSnackbar('Отправлено, без отслеживания', {
+        variant: 'warning',
       });
+      UpdateLawExec();
     }
   }, [AddAlert, Error, dispatch]);
+
   return (
     <>
       <Grid item>
