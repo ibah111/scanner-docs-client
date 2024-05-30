1feb1df1fed8b77c9fd002f3a983c00726258bbf
Ibah <starlord77@mail.ru>
2024-05-30 15:44:16 +0300

    feat: reset search and barcode

--- renderer/components/MainPage/SendComponents/Components/Up/Search/Reset.tsx
+++ renderer/components/MainPage/SendComponents/Components/Up/Search/Reset.tsx
@@ -1,13 +1,25 @@
 import { Button, Grid } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import ResetSendData from '../../../../../../utils/ResetSendData';
+import { useAppDispatch } from '../../../../../../Reducer';
+import { resetBarcodeState } from '../../../../../../Reducer/Barcode';
+import { resetSearch } from '../../../../../../Reducer/Search';
 
 export default function Reset() {
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
   return (
     <>
       <Grid item>
-        <Button onClick={ResetSendData}>{t('form.search.reset')}</Button>
+        <Button
+          onClick={() => {
+            ResetSendData();
+            dispatch(resetBarcodeState());
+            dispatch(resetSearch());
+          }}
+        >
+          {t('form.search.reset')}
+        </Button>
       </Grid>
     </>
   );
