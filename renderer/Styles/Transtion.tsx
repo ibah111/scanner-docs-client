355aab298bb68c4bc7cee0dbfb9e797747ecabdb
Ibah <starlord77@mail.ru>
2024-10-24 09:29:31 +0300

    feat: Add custom Tooltip

--- renderer/Styles/Transtion.tsx
+++ renderer/Styles/Transtion.tsx
@@ -1,4 +1,10 @@
-import { Slide } from '@mui/material';
+import {
+  Slide,
+  styled,
+  Tooltip,
+  tooltipClasses,
+  TooltipProps,
+} from '@mui/material';
 import { TransitionProps } from '@mui/material/transitions';
 import React from 'react';
 
@@ -10,3 +16,17 @@ export const Transition = React.forwardRef(function Transition(
 ) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
+
+export const StyledHtmlTooltip = styled(
+  ({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ),
+)(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    backgroundColor: '#f5f5f9',
+    border: '2px solid red',
+  },
+}));
