228d4cd2fd55ae11d623e5e493e792fc8378719a
Ibah <starlord77@mail.ru>
2024-10-29 15:39:40 +0300

    feat(DEPRECATED): visual update of old component
    Make this commit just for commit history

--- renderer/components/MainPage/SendComponents/Components/Up/Search/Comments.tsx
+++ renderer/components/MainPage/SendComponents/Components/Up/Search/Comments.tsx
@@ -10,20 +10,52 @@ import {
 } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useAppSelector } from '../../../../../../Reducer';
+import { useAppDispatch, useAppSelector } from '../../../../../../Reducer';
+import { setCommentProperty } from '../../../../../../Reducer/Comment';
+import UpdateComment from '../../../../../../apiSend/Comment/updateComments';
+import { enqueueSnackbar } from 'notistack';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import PriorityHighIcon from '@mui/icons-material/PriorityHigh';
 
 export default function Comments() {
+  const dispatch = useAppDispatch();
   const { t } = useTranslation();
   const [open, setOpen] = React.useState(false);
   const id = useAppSelector((state) => state.Send.id);
-  const LawActComment = useAppSelector((state) => state.Comment.LawAct);
-  const LawExecComment = useAppSelector((state) => state.Comment.LawExec);
+  const LawActComment = useAppSelector((state) => state.Comment.LawActComment);
+  const LawExecComment = useAppSelector(
+    (state) => state.Comment.LawExecComment,
+  );
+  const [prevLawActComment, setPrevNewLawActComment] =
+    React.useState<string>('');
+  const [prevLawExecComment, setPrevNewLawExecComments] =
+    React.useState<string>('');
+  const cancelChanges = React.useCallback(() => {
+    dispatch(setCommentProperty(['LawActComment', prevLawActComment]));
+    dispatch(setCommentProperty(['LawExecComment', prevLawExecComment]));
+  }, [dispatch, prevLawActComment, prevLawExecComment]);
+
   const handleClose = React.useCallback(() => {
     setOpen(false);
-  }, []);
+    cancelChanges();
+  }, [cancelChanges]);
   const handleOpen = React.useCallback(() => {
     setOpen(true);
   }, []);
+
+  const compare =
+    prevLawActComment === LawActComment &&
+    prevLawExecComment === LawExecComment;
+  const refresh = React.useCallback(() => {
+    setPrevNewLawActComment(LawActComment);
+    setPrevNewLawExecComments(LawExecComment);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  React.useEffect(() => {
+    if (open) {
+      refresh();
+    }
+  }, [open, refresh, prevLawActComment, prevLawExecComment]);
   return (
     <>
       <Grid item>
@@ -33,7 +65,18 @@ export default function Comments() {
       </Grid>
       <Dialog open={open} fullWidth maxWidth={'xl'} onClose={handleClose}>
         <DialogTitle variant="h4" sx={{ textAlign: 'center' }}>
-          {t('form.comments.title')}
+          {t('form.comments.title')}{' '}
+          {compare ? (
+            <>
+              <DoneAllIcon color="success" />
+            </>
+          ) : (
+            <>
+              <PriorityHighIcon color="warning" />
+              <PriorityHighIcon color="warning" />
+              <PriorityHighIcon color="warning" />
+            </>
+          )}
         </DialogTitle>
         <DialogContent>
           <Grid
@@ -45,18 +88,73 @@ export default function Comments() {
           >
             <Grid xs={6} item>
               <Typography variant="h6">{t('form.comments.law_act')}</Typography>
-              <TextField fullWidth multiline value={LawActComment} />
+              <TextField
+                fullWidth
+                multiline
+                value={LawActComment}
+                onChange={(event) => {
+                  const text = event.target.value;
+                  dispatch(setCommentProperty(['LawActComment', text]));
+                }}
+              />
             </Grid>
             <Grid xs={6} item>
               <Typography variant="h6">
                 {t('form.comments.law_exec')}
               </Typography>
-              <TextField fullWidth multiline value={LawExecComment} />
+              <TextField
+                fullWidth
+                multiline
+                value={LawExecComment}
+                onChange={(event) => {
+                  const text = event.target.value;
+                  dispatch(setCommentProperty(['LawExecComment', text]));
+                }}
+              />
             </Grid>
           </Grid>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>{t('form.comments.close')}</Button>
+          <Grid container spacing={1}>
+            <Grid item>
+              <Button
+                onClick={handleClose}
+                disabled={compare === false ? true : false}
+              >
+                {t('form.comments.close')}
+              </Button>
+            </Grid>
+            {compare === false && (
+              <>
+                <Grid item>
+                  <Button
+                    disabled={compare}
+                    onClick={cancelChanges}
+                    variant="outlined"
+                    color="error"
+                  >
+                    {'Reset changes'}
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    variant="contained"
+                    disabled={compare}
+                    onClick={() =>
+                      UpdateComment().subscribe(() => {
+                        enqueueSnackbar('Комментарий изменён', {
+                          variant: 'success',
+                        });
+                        handleClose();
+                      })
+                    }
+                  >
+                    {'Принять изменения'}
+                  </Button>
+                </Grid>
+              </>
+            )}
+          </Grid>
         </DialogActions>
       </Dialog>
     </>
