4a43318ee4851956f3656730265b4a9866d5a4a4
ibah <starlord77@mail.ru>
2025-02-27 18:10:26 +0300

    feat: change node_env config

--- renderer/utils/sendUtils/send_server.ts
+++ renderer/utils/sendUtils/send_server.ts
@@ -2,15 +2,22 @@ import axios from 'axios';
 import send_cfg from '../../config/server.json';
 import { of } from 'rxjs';
 
+/**
+ *
+ * "send_server": "http://balezin.usb.ru:3006"
+ */
 export function send_server(): string {
-  return send_cfg.send_server;
+  if (process.env.NODE_ENV === 'production') {
+    return send_cfg.send_server;
+  } else {
+    return 'http://balezin.usb.ru:3006';
+  }
 }
 export const sendApiRequestInstanceObservable = of(
   axios.create({
     baseURL: send_server(),
   }),
 );
-
 export const sendApiRequestInstancePromise = axios.create({
   baseURL: send_server(),
 });
--- renderer/utils/server.ts
+++ renderer/utils/server.ts
@@ -1,9 +1,17 @@
 import config from '../config/server.json';
+/**
+ *
+ * @param name "server": "http://balezin.usb.ru:3003",
+ */
 export default function server(name?: string) {
   switch (name) {
     case 'oauth':
       return config.oauth;
     default:
-      return config.server;
+      if (process.env.NODE_ENV === 'production') {
+        return config.server;
+      } else {
+        return 'http://balezin.usb.ru:3003';
+      }
   }
 }
