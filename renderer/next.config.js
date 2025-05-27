32770b7812d421415b3ebf02745dda7fd9385805
ibah <starlord77@mail.ru>
2025-05-27 13:52:36 +0300

    fix: for build

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -5,4 +5,14 @@ module.exports = {
   webpack: (config) => {
     return config;
   },
+  // Увеличиваем таймаут сборки
+  staticPageGenerationTimeout: 120,
+  // Отключаем некоторые оптимизации для ускорения сборки
+  swcMinify: false,
+  reactStrictMode: false,
+  // Увеличиваем лимит памяти
+  experimental: {
+    optimizeCss: false,
+    optimizePackageImports: false,
+  }
 };
